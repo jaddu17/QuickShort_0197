56a2349f870b824605289c9e0f67c9983ae20b5e
jaddu17 <shafajaddu@gmail.com>
2024-03-28 09:43:48 +0700

    perintah tukar elemen pada indeks I dengan elemen pada indeks J

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -65,5 +65,10 @@ void q_short(int low, int high)
 			cmp_count++;
 		}
 		cmp_count++;
+		if (i < j)			// step 9
+		{
+			// swap the element at index i with the element at index j
+			swap(i, j);
+		}
 	}
 }
