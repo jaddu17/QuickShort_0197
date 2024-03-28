62d89a41b4d7f29c350f0a37b18ec0310a7d8426
jaddu17 <shafajaddu@gmail.com>
2024-03-28 09:55:55 +0700

    membuat prosedur memanggil

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -95,3 +95,13 @@ void display() {
 	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
 	cout << "Number of data movements: " << mov_count << endl;
 }
+int main()
+{
+	char ch;
+
+	do {
+		input();
+		q_short(0, n - 1);
+		display();
+	}
+}
